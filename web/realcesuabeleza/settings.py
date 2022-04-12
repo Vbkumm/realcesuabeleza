24387e08906b47ca8d01ea23bab7c37e97264245
Vinicius Bittencourt Kumm <vncsbk@hotmail.com>
2022-04-12 08:57:31 -0300

    updatesetings2

--- web/realcesuabeleza/settings.py
+++ web/realcesuabeleza/settings.py
@@ -42,11 +42,12 @@ INSTALLED_APPS = [
     'rest_framework',
 
     #own
+    'main',
     'accounts',
     'businesses',
     'customers',
     'professionals',
-    'main',
+
 ]
 
 MIDDLEWARE = [
@@ -83,13 +84,24 @@ WSGI_APPLICATION = 'realcesuabeleza.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+os.environ.get('DJANGO_SECRET_KEY')
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': os.environ.get('POSTGRES_PORT')
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators

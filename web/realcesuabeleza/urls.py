2aca9d8015f94545c3b7f27494034734c286e7f1
Vinicius Bittencourt Kumm <vncsbk@hotmail.com>
2022-05-12 13:12:49 -0300

    allauth

--- web/realcesuabeleza/urls.py
+++ web/realcesuabeleza/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from allauth.account.views import confirm_email
 from main.views import home_view
 
 
@@ -25,4 +27,10 @@ urlpatterns = [
     path('', include('customers.urls')),
     path('', include('professionals.urls')),
     path('', include('services.urls')),
+
+    url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
+    url(r'^account/', include('allauth.urls')),
+    url(r'^accounts-rest/registration/account-confirm-email/(?P<key>.+)/$', confirm_email, name='account_confirm_email'),
+
 ]

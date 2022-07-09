8731a4ab1086026118a20d654970b4330e93d250
Vinicius Bittencourt Kumm <vncsbk@hotmail.com>
2022-07-09 10:41:04 -0300

    test_user

--- web/accounts/tests.py
+++ web/accounts/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import CustomUserManager
+from django.test import Client
 
 # Create your tests here.
+
+
+class CustomUserTestCase(TestCase):
+    def setUp(self):
+        self.user = CustomUserManager.objects.create(email='test@email.com', password='')
+        self.user.set_password('secret')
+        self.user.save()
+        self.client = Client()
+        self.client.login(email='test@email.com', password='secret')
